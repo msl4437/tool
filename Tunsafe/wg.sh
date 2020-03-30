584bc21ed7dabc446cf5e6534b022faf3dd69f39
msl4437 <16162839@qq.com>
2020-03-30 19:57:07 +0800

    Add files via upload

---
+++ Tunsafe/wg.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+config_dir="$HOME/.wireguard/"
+
+mkdir -p "$config_dir"
+cd "$config_dir" || {
+    echo 切换目录失败，程序退出
+    exit
+}
+# 生成两对密钥，分别用作服务器和客户端使用
+wg genkey | tee pri1 | wg pubkey >pub1
+wg genkey | tee pri2 | wg pubkey >pub2
+
+# 设置密钥访问权限
+chmod 600 pri1
+chmod 600 pri2
+
+interface=$(ip -o -4 route show to default | awk '{print $5}')
+ip="$( wget -qO- -t1 -T2 ipv4.icanhazip.com )"
+sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+
+
+# 生成服务端配置文件
+cat >wg0.conf <<EOL
+[Interface] 
+PrivateKey = $(cat pri1)
+Address = 172.16.0.1/24
+ListenPort = 51820
+PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $interface -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $interface -j MASQUERADE
+[Peer]
+PublicKey = $(cat pub2)
+AllowedIPs = 172.16.0.2/32
+EOL
+
+# 生成客户端配置文件
+cat >client.conf <<EOL
+[Interface]
+PrivateKey = $(cat pri2)
+Address = 172.16.0.2/24
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = $(cat pub1)
+Endpoint = $ip:51820
+AllowedIPs = 0.0.0.0/0
+EOL
+
+# 复制配置文件并启动
+sudo cp wg0.conf /etc/wireguard/ || {
+    echo 复制失败,请检查/etc/wireguard目录或wg0.conf是否已经存在
+    exit
+}
+sudo wg-quick up wg0 || {
+    echo 启动wireguard失败，请检查/etc/wireguard/wg0.conf是否存在错误
+    exit
+}
+
+# 显示客户端配置文件
+echo "----------以下是客户端配置文件，请保存并在客户端中使用----------"
+cat client.conf
